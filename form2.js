67eee1cb760f710451216a0a0152447305072705
annachiara97 <74875500+annachiara97@users.noreply.github.com>
2022-05-29 17:09:06 +0200

    ghghg

--- form2.js
+++ form2.js
@@ -32,7 +32,7 @@ document.querySelector('.alert').style.display = "block";
 // remove the alert
 setTimeout(() => {
     document.querySelector('.alert').style.display = "none";
-}, 3000);
+}, 4000);
 
 
 // reset the form
